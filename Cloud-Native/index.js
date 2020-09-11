244d7b84311cf2d94288d545015d80666fb0299b
Immanuella Busari <immanuela@mentormate.com>
2020-09-11 18:56:46 +0100

    It's working

--- Cloud-Native/index.js
+++ Cloud-Native/index.js
@@ -1,24 +1,50 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 const server = express();
-const port = 3000;
+const fs = require('fs');
+const formidable = require('formidable')
 
-
-server.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
-server.use(bodyParser.json());
-server.use(express.static(__dirname + '/public'));
-
-server.get('/', (req, res) => {
-    res.send('Routing works');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey
 });
 
 server.post('/upload', (req, res) => {
+    const form = formidable({ multiples: true });
 
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.end("Couldn't do it.")
+            return;
+        }
+
+        let file = files.fileName;
+        let fileName = files.fileName.name;
+
+        console.log('Uploading file...');
+        // Read content from the file
+        //  const fileContent = fs.readFileSync(fileName);
+        let fileStream = fs.createReadStream(file.path)
+            .on('error', function(err) {
+                console.log('File Error', err);
+            });
+
+        // Setting up S3 upload parameters
+        const params = {
+            Bucket: 'immanuella-busari-devcamp-mm',
+            Key: fileName, // File name you want to save as in S3
+            Body: fileStream
+        };
+
+        // Uploading files to the bucket
+        s3.upload(params, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+            res.json({ fields, files });
+        });
+    });
 });
 
 
